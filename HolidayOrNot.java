4ef7b2f1824ddf7a4059146a84edcd1c13b411fc
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-09-30 11:46:08 +0530

    Create HolidayOrNot.java

---
+++ HolidayOrNot.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class HolidayOrNot {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+			switch(str){
+
+			case "Monday":
+				System.out.println("no");
+				break;
+			case "Tuesday":
+				System.out.println("no");
+				break;
+			case "Wednesday":
+				System.out.println("no");
+				break;
+			case "Thursday":
+				System.out.println("no");
+				break;
+			case "Friday":
+				System.out.println("no");
+				break;
+			case "Saturday":
+				System.out.println("no");
+				break;
+			case "Sunday":
+				System.out.println("yes");
+				break;
+			}
+			
+		}
+
+	}
