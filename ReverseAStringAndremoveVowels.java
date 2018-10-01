729487ab1ae3671b6e2e6011d31c3a44d1044ea7
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-10-01 16:26:18 +0530

    Update ReverseAStringAndremoveVowels.java

--- ReverseAStringAndremoveVowels.java
+++ ReverseAStringAndremoveVowels.java
@@ -4,8 +4,8 @@ public class ReverseAndRemoveVowels {
 	public static void main(String[] args) {
 	Scanner s=new Scanner(System.in);
 	String rev="",str1="";
-	int n=s.nextInt();
-		String str=s.next();
+	String n=s.nextLine();
+		String str=s.nextLine();
 		for(int i=0;i<str.length();i++){
 			if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'){
 				continue;
