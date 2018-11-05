e515661fde6995fd4d78bd7c10748ee4d65f61ae
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-11-05 22:16:26 +0530

    Update and rename DifferBykChar.java to DiffersBykChar.java

--- DiffersBykChar.java
+++ DiffersBykChar.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
-public class DiffersByOneChar {
+public class DiffersBykChar
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		Scanner s=new Scanner(System.in);
 		String str=s.next();
 		String str2=s.next();
@@ -9,15 +11,20 @@ public class DiffersByOneChar {
 		int counter=0;
 		char[] s1=str.toCharArray();
 		char [] s2=str2.toCharArray();
-		for(int i=0;i<str.length();i++){
-			if(!str2.contains(String.valueOf(str.charAt(i)))||!str.contains(String.valueOf(str2.charAt(i)))){
+		for(int i=0;i<str.length();i++)
+		{
+			if(!str2.contains(String.valueOf(str.charAt(i)))||!str.contains(String.valueOf(str2.charAt(i))))
+				{
 					counter++;
 				}
-			}
+		}
 		
-		if(counter==n){
+		if(counter==n)
+		{
 			System.out.println("yes");
-		}else{
+		}
+		else
+		{
 			System.out.println("no");
 		}
 	}
