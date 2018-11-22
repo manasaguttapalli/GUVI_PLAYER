af3e74f1489929f9dca9cc031cba224d6f01712f
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-11-22 19:10:45 +0530

    Update and rename SumOfTheNegativeNumbers.java to SumOfNegativeNumbersInArray.java

--- SumOfNegativeNumbersInArray.java
+++ SumOfNegativeNumbersInArray.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class SumOfTheNegativeNumbers {
+public class SumOfNegativeNumbersInArray {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
