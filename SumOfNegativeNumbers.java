26ab152a5ee9aceb44b51ad03cb29222e6098f29
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-11-22 19:05:16 +0530

    Create SumOfNegativeNumbers.java

---
+++ SumOfNegativeNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class SumOfNegativeNumbers {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int sum=0;
+		int a[]=new int[N];
+		for(int i=0;i<N;i++){
+			a[i]=s.nextInt();
+		}
+		for(int i=0;i<N;i++){
+			if(a[i]<0){
+				sum=a[i]+sum;
+			}
+		}
+		System.out.print(sum);
+	}
+
+}
