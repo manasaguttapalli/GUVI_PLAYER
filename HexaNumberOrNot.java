fa9ee3d1fffac6ac5e9bdb124114ecfea3d4c22f
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-11-22 20:19:05 +0530

    Create HexaNumberOrNot.java

---
+++ HexaNumberOrNot.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class HexaNumberOrNot {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int count=0;
+		String str=s.nextLine();
+		for(int i=0;i<str.length();i++){
+			if(str.charAt(i)=='A'||str.charAt(i)=='B'||str.charAt(i)=='C'||str.charAt(i)=='D'||str.charAt(i)=='E'||str.charAt(i)=='F'||str.charAt(i)>'0' && str.charAt(i)<='9'){
+				count++;
+			}
+		}
+	
+		if(count-str.length()==0)
+			System.out.println("yes");
+
+		else
+			System.out.println("no");
+	}
+
+}
