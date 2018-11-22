56b96eee5de4aadfc445a11193b6767117c3d02b
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-11-22 11:44:07 +0530

    Create CatalanNumber.java

---
+++ CatalanNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class CatalanNumbers {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		for(int i=0;i<=N-1;i++){
+		System.out.print(res(i)+" ");
+		}
+		
+		System.out.print(res(N));
+
+	}
+
+	private static int res(int n) {
+		int result=0;
+		if(n<=1)
+			return 1;
+		for(int i=0;i<n;i++){
+			result+=res(i)*res(n-i-1);
+		}
+			
+		return result;
+		
+	}
+
+}
